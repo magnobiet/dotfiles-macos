c336a8764a046ac3ed702c55324c71710d777538
Magno F. B. Biét <magnobiet@users.noreply.github.com>
2025-02-23 11:30:33 -0300

    chore: fix set global versions

--- asdf.sh
+++ asdf.sh
@@ -38,26 +38,26 @@ source $HOME/.zshrc
 
 asdf plugin add nodejs
 asdf install nodejs 22.14.0
-asdf global nodejs 22.14.0
+asdf set -u nodejs 22.14.0
 
 # asdf plugin add dart https://github.com/patoconnor43/asdf-dart.git
 # asdf install dart 3.1
-# asdf global dart 3.1
+# asdf set -u dart 3.1
 
 # asdf plugin add flutter
 # asdf install flutter 3.29.0
-# asdf global flutter 3.29.0
+# asdf set -u flutter 3.29.0
 
 asdf plugin add python
 asdf install python 3.13.2
-asdf global python 3.13.2
+asdf set -u python 3.13.2
 
 # asdf plugin add golang https://github.com/kennyp/asdf-golang.git
 # asdf install golang 1.24.0
-# asdf global golang 1.24.0
+# asdf set -u golang 1.24.0
 
 # asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 # asdf install ruby 3.4.2
-# asdf global ruby 3.4.2
+# asdf set -u ruby 3.4.2
 
 source $HOME/.zshrc
